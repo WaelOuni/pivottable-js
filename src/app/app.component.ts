043bc247b4b62d3ed389711def07f7bb83ebf8e6
wael <wael.ouni.partner@decathlon.com>
2018-10-03 16:17:37 +0200

    add pivottableJs with angular 6 project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, Input, Inject } from '@angular/core';
+
+declare var jQuery: any;
+declare var $: any;
+import 'pivottable/dist/pivot.min.js';
+import 'pivottable/dist/pivot.min.css';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pivot-table-js-example';
+  private el: ElementRef;
+  constructor(@Inject(ElementRef) el: ElementRef) {
+    this.el = el;
+  }
+
+  ngAfterViewInit() {
+    let container = this.el.nativeElement;
+    let inst = jQuery(container);
+    let targetElement = inst.find("#output");
+    targetElement.pivotUI(
+      //data : json array of fake objects
+      [{ "id": "1001", "name": "Wael", "account": "3900", "max_per_week": "600", "is_gold": true },
+      { "id": "1002", "name": "Alex", "account": "2490", "max_per_week": "400", "is_gold": false },
+      { "id": "1003", "name": "Romain", "account": "5775", "max_per_week": "800", "is_gold": true },
+      { "id": "1004", "name": "Saif", "account": "2233", "max_per_week": "360", "is_gold": false },
+      { "id": "1005", "name": "Patrick", "account": "56", "max_per_week": "10" }],
+      {
+        // initial colomns and rows of the pivot table
+        cols: ["max_per_week", "account"], rows: ["id", "name"]
+      });
+  }
 }
